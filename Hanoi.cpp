ac3fe7916840b815f6b1af1121991b999125c54b
K2ot <krzysztof.e.kot@gmail.com>
2023-01-06 19:15:54 +0100

    Initial commit

---
+++ Hanoi.cpp
@@ -0,0 +1,24 @@
+﻿// Hanoi.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+double i = 0;
+
+void hanoi(int n, char A, char B, char C)
+{
+    // przekłada n krążków z A korzystając z B na C
+    if (n > 0)
+    {
+        hanoi(n - 1, A, C, B);
+        std::cout << A << " -> " << C <<" "<<++i<< std::endl;
+        hanoi(n - 1, B, A, C);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    hanoi(3, 'A', 'B', 'C');
+    std::cout << i << std::endl;
+    return 0;
+
+}
